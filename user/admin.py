133a66262dad8a4fd6192e5fe5c889bb51b909d1
Nikitha2404 <hebbar.nikitha@gmail.com>
2024-03-03 18:23:24 +0530

    changes

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Student,Client,Course,Lesson,Material,FirstQuiz,Questionnaire,MaterialProgress,Progress,Feedback
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Client)
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Material)
+admin.site.register(FirstQuiz)
+admin.site.register(Questionnaire)
+admin.site.register(MaterialProgress)
+admin.site.register(Progress)
+admin.site.register(Feedback)
