b6c937c72da09516491865a73ef5b5e1e60d002a
1RN20CS070-KshamaIyer <1rn20cs070.kshamaiyer@gmail.com>
2024-03-03 18:03:29 +0530

    model

--- init.sql
+++ init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS student (id serial PRIMARY KEY, email varchar(1000),pwd varchar(65535),username varchar(1000), learnstyle varchar(1000));
+CREATE TABLE IF NOT EXISTS student (id serial PRIMARY KEY, email varchar(1000),pwd varchar(65535),username varchar(1000) unique, learnstyle varchar(1000));
 
-CREATE TABLE IF NOT EXISTS client (id serial PRIMARY KEY, email varchar(1000),pwd varchar(65535),clientname varchar(1000));
+CREATE TABLE IF NOT EXISTS client (id serial PRIMARY KEY, email varchar(1000),pwd varchar(65535),clientname varchar(1000) unique);
 
 CREATE TABLE IF NOT EXISTS firstquiz (id serial PRIMARY KEY, question varchar(2000),choice1 varchar(1000),choice2 varchar(1000),choice3 varchar(1000),choice4 varchar(1000));
 
-CREATE TABLE IF NOT EXISTS course (id serial PRIMARY KEY, coursename varchar(1000),coursedescription varchar(65535),CONSTRAINT clientid FOREIGN KEY (id) REFERENCES client(id) ON DELETE CASCADE);
+CREATE TABLE IF NOT EXISTS course (id serial PRIMARY KEY, coursename varchar(1000) unique,coursedescription varchar(65535),CONSTRAINT clientid FOREIGN KEY (id) REFERENCES client(id) ON DELETE CASCADE);
 
 CREATE TABLE IF NOT EXISTS lesson (id serial PRIMARY KEY, lessonname varchar(1000),lessondescription varchar(65535),CONSTRAINT courseid FOREIGN KEY (id) REFERENCES course(id) ON DELETE CASCADE);
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,96 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    email = models.CharField(max_length=1000)
+    pwd = models.CharField(max_length=65535)
+    username = models.CharField(max_length=1000,unique=True)
+    learnstyle = models.CharField(max_length=1000)
+
+    def __str__(self) -> str:
+        return self.username
+
+class Client(models.Model):
+    email = models.CharField(max_length=1000)
+    pwd = models.CharField(max_length=65535)
+    clientname = models.CharField(max_length=1000,unique=True)
+
+    def __str__(self) -> str:
+        return self.clientname
+
+class FirstQuiz(models.Model):
+    question = models.CharField(max_length=2000)
+    choice1 = models.CharField(max_length=1000)
+    choice2 = models.CharField(max_length=1000)
+    choice3 = models.CharField(max_length=1000)
+    choice4 = models.CharField(max_length=1000)
+
+    def __str__(self) -> str:
+        return self.question
+
+class Course(models.Model):
+    coursename = models.CharField(max_length=1000,unique=True)
+    coursedescription = models.CharField(max_length=65535)
+    clientid = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.coursename
+
+class Lesson(models.Model):
+    lessonname = models.CharField(max_length=1000)
+    lessondescription = models.CharField(max_length=65535)
+    courseid = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+       return self.lessonname
+
+class Material(models.Model):
+    visual = models.CharField(max_length=65535)
+    auditory = models.CharField(max_length=65535)
+    reading = models.CharField(max_length=65535)
+    difficulty = models.CharField(max_length=1000)
+    lessonid = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+       return self.difficulty
+
+class Questionnaire(models.Model):
+    question = models.CharField(max_length=65535)
+    choice1 = models.CharField(max_length=1000)
+    choice2 = models.CharField(max_length=1000)
+    choice3 = models.CharField(max_length=1000)
+    choice4 = models.CharField(max_length=1000)
+    answer = models.CharField(max_length=65535)
+    difficulty = models.CharField(max_length=1000)
+    lessonid = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    materialid = models.ForeignKey(Material, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.question
+
+class MaterialProgress(models.Model):
+    notattempted = models.BooleanField()
+    inprogress = models.BooleanField()
+    completed = models.BooleanField()
+    studentid = models.ForeignKey(Student, on_delete=models.CASCADE)
+    lessonid = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    materialid = models.ForeignKey(Material, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.studentid
+
+
+class Progress(models.Model):
+    progress = models.DecimalField()
+    studentid = models.ForeignKey(Student, on_delete=models.CASCADE)
+    courseid = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.studentid+' '+self.progress
+
+class Feedback(models.Model):
+    mood = models.CharField(max_length=65535)
+    studentid = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.studentid+' '+self.mood
